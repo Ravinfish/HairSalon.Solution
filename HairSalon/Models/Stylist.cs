1ad063ee3f08cfe0c2662c8d6b3592057bbf8664
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-07 19:37:59 -0800

    add to client and stylist models. updated client index controller/view

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models;
+
+public class Stylist
+{
+  public int StylistId { get; set; }
+  public string Name { get; set;}
+  public string Specialty { get; set; }
+}
