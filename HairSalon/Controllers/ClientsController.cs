d6de041ff89b6b53bb4eb3ac80ff5446a960675d
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-09 14:28:40 -0800

    updated client controller to add stylist, added client details/create/index

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,6 +3,8 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
@@ -15,18 +17,25 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients
+      .Include(client => client.Stylist)
+      .ToList();
       return View(model);
     }
 
     public ActionResult Create()
     {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
 
     [HttpPost]
     public ActionResult Create(Client client)
     {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
       _db.Clients.Add(client);
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -34,13 +43,16 @@ namespace HairSalon.Controllers
 
      public ActionResult Details(int id)
     {
-      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      Client thisClient = _db.Clients
+      .Include(client => client.Stylist)
+      .FirstOrDefault(client => client.ClientId == id);
       return View(thisClient);
     }
 
     public ActionResult Edit(int id)
     {
       Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View(thisClient);
     }
 
